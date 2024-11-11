40b7fdb0e4ce5ef97eeabadaf904f5dd123bcc97
Berthony Sully <31166555+htsull@users.noreply.github.com>
2024-11-11 05:42:47 -0500

    update tools
    add acernity ui

--- tailwind.config copy.ts
+++ tailwind.config copy.ts
@@ -1,12 +1,5 @@
 import type { Config } from 'tailwindcss'
 
-const svgToDataUri = require('mini-svg-data-uri')
-
-const colors = require('tailwindcss/colors')
-const {
-  default: flattenColorPalette
-} = require('tailwindcss/lib/util/flattenColorPalette')
-
 const config: Config = {
   darkMode: ['class'],
   content: [
@@ -72,11 +65,6 @@ const config: Config = {
         sm: 'calc(var(--radius) - 4px)'
       },
       keyframes: {
-        scroll: {
-          to: {
-            transform: 'translate(calc(-50% - 0.5rem))'
-          }
-        },
         'accordion-down': {
           from: {
             height: '0'
@@ -96,48 +84,10 @@ const config: Config = {
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        scroll:
-          'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite'
+        'accordion-up': 'accordion-up 0.2s ease-out'
       }
     }
   },
-  plugins: [
-    require('tailwindcss-animate'),
-    addVariablesForColors,
-    function ({ matchUtilities, theme }: any) {
-      matchUtilities(
-        {
-          'bg-grid': (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`
-          }),
-          'bg-grid-small': (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`
-          }),
-          'bg-dot': (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`
-          })
-        },
-        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
-      )
-    }
-  ]
+  plugins: [require('tailwindcss-animate')]
 }
 export default config
-
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme('colors'))
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  )
-
-  addBase({
-    ':root': newVars
-  })
-}
